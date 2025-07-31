6980227b478538cbe81cbc99ab06466ca30b070c
piyush katare <piyushkatare18@gmail.com>
2025-07-31 11:16:57 +0530

    register

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import { register, login, getUser,googleLogin } from '../controller/user.controller.js'
-import cors from 'cors';
-app.use(cors({ origin: ['http://localhost:5173', 'https://hor123.netlify.app'], credentials: true }));
 
     const route = express.Router();
 
